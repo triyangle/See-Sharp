14570ccd17744d0e6fc297216e29186e63825055
William Yang <willy.h.yang@gmail.com>
2017-02-19 03:54:23 -0800

    Add sample VR demos

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = new Vector3 (target.position.x, transform.position.y, target.position.z);
+	}
+}

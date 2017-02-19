1c873f9d077e51217d9564cda649d01a1fb0d63a
William Yang <willy.h.yang@gmail.com>
2017-02-19 01:01:52 -0800

    Added duck and fixed piano

---
+++ Assets/Scripts/RandomMoveScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMoveScript : MonoBehaviour {
+	// Update is called once per frame
+	public Rigidbody duck;
+	void Update () {
+		if (Random.value > .5) {
+			duck.velocity = new Vector3(100 * Random.value, duck.velocity.y, duck.velocity.z);
+		}
+		else {
+			duck.velocity = new Vector3(-100 * Random.value, duck.velocity.y, duck.velocity.z);
+		}
+		if (Random.value > .5) {
+			duck.velocity = new Vector3 (duck.velocity.x, duck.velocity.y, 100 * Random.value);
+		}
+		else {
+			duck.velocity = new Vector3 (duck.velocity.x, duck.velocity.y, -100 * Random.value);
+		}
+	}
+}

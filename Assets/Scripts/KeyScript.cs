3bf360628bd28dab0bae43bdfd50c5242c30df0a
William Yang <willy.h.yang@gmail.com>
2017-02-19 00:06:19 -0800

    Add initial treehacks submission

---
+++ Assets/Scripts/KeyScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyScript : MonoBehaviour {
+
+	private AudioSource a;
+	public string letter;
+	public float timer = 0.0f;
+	public float pressDownTime = 0.2f;
+	public float startPos;
+	// Use this for initialization
+	void Start () {
+		a = GetComponent<AudioSource>();
+		startPos = transform.position.y;
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(letter)) {
+			timer = pressDownTime;
+			a.Play ();
+		}
+
+		if (timer > 0) {
+			timer -= Time.deltaTime;
+		}
+		if (timer < 0) {
+			timer = 0;
+		}
+		transform.position = new Vector3(transform.position.x, 
+							 Mathf.Lerp(startPos, startPos + 0.20f, (pressDownTime - timer) / pressDownTime), 
+							 transform.position.z); 
+	
+	}
+}
---
+++ Assets/TargetCameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetCameraFollow : MonoBehaviour {
+
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = new Vector3(target.position.x, transform.position.y, target.position.z);
+	}
+}
